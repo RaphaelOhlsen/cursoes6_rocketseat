bab12617a9115eee767fd0afdfd6949d3fdc7824
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-05-25 20:54:58 -0300

    Valores padrão

--- bundle.js
+++ bundle.js
@@ -1,36 +1,6 @@
-"use strict";
+//Usado para definir valores padrões para uma funçao
 
-var arr = [1, 3, 4, 5, 8, 9]; // Método 'MAP' percorre um vetor e consegue-se
-//  manipular os elementos do vetor
+const soma = (a = 3, b = 2) => a + b
 
-var newArr = arr.map(function (item, index) {
-  return item + index;
-});
-console.log(newArr); // Método 'RECDUCE' reune todos os elementos do vetor e 
-// transforma em uma unica informação
-
-var letters = ['R', 'a', 'p', 'h', 'a', 'e', 'l'];
-var word = letters.reduce(function (total, next) {
-  return total + next;
-});
-console.log(word); // Método 'FILTER' filtra um vetor e retorna no novo vetor
-// somente so valores que passam por análise de verdadeiro 
-
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter); // Método 'FIND' usado para emcontrar um elemento em um vetor
-
-var find = arr.find(function (item) {
-  return item === 4;
-});
-console.log(find);
-
-var teste = function teste() {
-  return {
-    nome: 'Raphael',
-    sobrenome: 'Ohlsen'
-  };
-};
-
-console.log(teste().sobrenome);
+console.log(soma(1))
+console.log(soma())
