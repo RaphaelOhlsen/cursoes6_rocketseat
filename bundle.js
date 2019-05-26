f93aa0217b5fdf2633bd3592490b32ee18ed6a7a
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-05-26 01:01:49 -0300

    Desafio 6

--- bundle.js
+++ bundle.js
@@ -1,34 +1,5 @@
 "use strict";
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-// 5.1
-var arr = [1, 2, 3, 4, 5, 6];
-var x = arr[0],
-    y = arr.slice(1);
-console.log(x);
-console.log(y); // 5.2
-
-var usuario = {
-  nome: 'Diego',
-  idade: 23,
-  endereco: {
-    cidade: 'Rio do Sul',
-    uf: 'SC',
-    pais: 'Brasil'
-  }
-};
-
-var usuario2 = _objectSpread({}, usuario, {
-  nome: 'Gabriel'
-});
-
-var usuario3 = _objectSpread({}, usuario, {
-  cidade: 'Lontras'
-});
-
-console.log(usuario);
-console.log(usuario2);
-console.log(usuario3);
+var usuario = 'Diego';
+var idade = 23;
+console.log("O usu\xE1rio ".concat(usuario, " possui ").concat(idade, " anos"));
--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-// 5.1
-
-const arr = [1, 2, 3, 4, 5, 6]
-
-const [x, ...y] = arr
-
-console.log(x)
-console.log(y)
-
-// 5.2
-
-const usuario = {
-  nome: 'Diego',
-  idade: 23,
-  endereco: {
-  cidade: 'Rio do Sul',
-  uf: 'SC',
-  pais: 'Brasil',
-  }
- }
-
- const usuario2 = { ...usuario, nome: 'Gabriel'}
-
- const usuario3 = { ...usuario, cidade: 'Lontras'}
-
- console.log(usuario)
- console.log(usuario2)
- console.log(usuario3)
+const usuario = 'Diego';
+const idade = 23;
+console.log(`O usuário ${usuario} possui ${idade} anos`)
