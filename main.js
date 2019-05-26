644b2dc4ca41d24da83cf119923791039e584fd6
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-05-25 23:12:34 -0300

    Template Literals

--- main.js
+++ main.js
@@ -1,57 +1,6 @@
-//------//
-// REST //
-//------//
+const nome = 'Raphael'
+const idade = 47
 
-// 1
-const usuario = {
-  nome: 'Raphael',
-  idade: 48,
-  empresa: 'Nexel'
-}
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`)
 
-const { nome, ...resto } = usuario
-
-console.log(nome)
-console.log(resto)
-
-// 2
-const arr = [1, 2, 3, 4]
-
-const [ a, b, ...c] = arr
-
-console.log(a)
-console.log(b)
-console.log(c)
-
-// 3
-const soma = (...parans) => parans.reduce((total, next) => total + next)
-
-
-console.log(soma(1,2,3,4))
-
-//--------//
-// SPREAD //
-//--------//
-
-// 1
-const arr1 = [1, 2, 3]
-const arr2 = [4, 5, 6]
-
-const arr3 = [...arr1, ...arr2]
-
-console.log(arr3)
-
-// 2
-const user1 = {
-  nome: 'Raphael',
-  idade: 48, 
-  empresa: 'Nexel'
-}
-
-const user2 = {
-  ...user1,
-  nome: 'Mario',
-  cidade: 'Vitória'
-}
-
-console.log(user2)
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos')
