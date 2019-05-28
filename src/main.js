7e5954fb954f2ba3c78ada3f655354846c674bad
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-05-28 11:24:57 -0300

    Aplicação ES6

--- src/main.js
+++ src/main.js
@@ -1,14 +1,87 @@
-const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+import api from './api';
 
-async function umPorSegundo() {
-  await delay();
-  console.log("1s");
+class App {
+  constructor() {
+    this.repositories = []
+    this.formEl = document.getElementById('repo-form')
+    this.inputEl = document.querySelector('input[name=repository]')
+    this.listEl = document.getElementById('repo-list')
+    this.registerHandlers()
+  }
 
-  await delay();
-  console.log("2s");
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event)
+  }
 
-  await delay();
-  console.log("3s");
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement('span')
+      loadingEl.appendChild(document.createTextNode('Carregando'))
+      loadingEl.setAttribute('id', 'loading')
+      this.formEl.appendChild(loadingEl)
+    } else {
+      document.getElementById('loading').remove()
+    }
+  }
+
+  async addRepository(event) {
+    event.preventDefault()
+
+    const repoInput = this.inputEl.value
+
+    if (repoInput.length === 0) return
+
+    this.setLoading()
+
+    try {
+      const response = await api.get(`repos/${repoInput}`)
+
+      console.log(response)
+  
+      const { name, description, html_url, owner: { avatar_url } }= response.data
+  
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      })
+  
+      this.render();
+    } catch (err){
+      alert('O repositório não existe')
+    }
+   
+    this.setLoading(false)
+  }
+
+  render() {
+    this.inputEl.value = ''
+    this.listEl.innerHTML = ''
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img')
+      imgEl.setAttribute('src', repo.avatar_url)
+
+      let titleEl = document.createElement('strong')
+      titleEl.appendChild(document.createTextNode(repo.name))
+
+      let descriptionEl = document.createElement('p')
+      descriptionEl.appendChild(document.createTextNode(repo.description))
+
+      let linkEl = document.createElement('a')
+      linkEl.setAttribute('target', '_blank')
+      linkEl.setAttribute('href', repo.html_url)
+      linkEl.appendChild(document.createTextNode('Acessar'))
+
+      let listItemEl = document.createElement('li')
+      listItemEl.appendChild(imgEl, titleEl, descriptionEl, linkEl)
+      listItemEl.appendChild(titleEl)
+      listItemEl.appendChild(descriptionEl)
+      listItemEl.appendChild(linkEl)
+
+      this.listEl.appendChild(listItemEl)
+    })
+  }
 }
 
-umPorSegundo()
+new App()
